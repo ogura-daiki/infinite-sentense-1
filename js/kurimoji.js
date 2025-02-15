b9fec5053c47d0fc85b91806e15b9a38cb86555e
OGURA DAIKI <dkog2001@gmail.com>
2025-02-15 12:31:59 +0900

    github pagesにてkiromoji.jsのローディングエラーが発生する問題への対応2

--- js/kurimoji.js
+++ js/kurimoji.js
@@ -1,8 +1,14 @@
+import { create } from "./create.js";
+const docBase = create("base", {href:"https://"});
+document.head.append(docBase);
+
 const buildKuromoji = () => new Promise(r => {
   kuromoji.builder({ dicPath: "https://cdn.jsdelivr.net/npm/kuromoji@0.1.2/dict/" }).build(function (err, tokenizer) {
     r(tokenizer);
   })
 });
 const kuromojiTokenizer = await buildKuromoji();
+
+docBase.remove();
 
 export {kuromojiTokenizer};
